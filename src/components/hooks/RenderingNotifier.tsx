f831c5c493234636cafdbacf20132d19387b4f61
Hiroharu Tanaka <hiroharu8864@gol.com>
2022-07-13 17:06:19 +0900

    Suspense動作確認

---
+++ src/components/hooks/RenderingNotifier.tsx
@@ -0,0 +1,6 @@
+import { FC, memo } from "react";
+
+export const RenderingNotifier: FC<Props> = memo((Props) => {
+  const { name } = Props;
+  return null;
+});
--- src/components/pages/TestPage.tsx
+++ src/components/pages/TestPage.tsx
@@ -1,17 +1,40 @@
-import { FC, memo } from "react";
+import { FC, memo, Suspense, useState } from "react";
 
 export const TestPage: FC = memo(() => {
-  const sleep = (ms: number) => {
+  /**
+   * Promiseは非同期処理を表すオブジェクトのこと。
+   * 非同期処理終了後に解決(settle)する。
+   * コンポーネントがPromiseをthrowした場合、
+   * そのコンポーネントはサスペンドとなる。
+   */
+  const throwPromise = (ms: number) => {
     return new Promise((resolve) => setTimeout(resolve, ms));
   };
+  /**
+   * 1秒後にPromiseを返してサスペンドを発生させる。
+   */
   const AlwaysSuspend: FC = () => {
-    throw sleep(1000);
+    // console.log("Always Suspend is rendered.")
+    throw throwPromise(1000);
   };
+
+  const SometimesSuspend: FC = () => {
+    if (Math.random() < 0.5) {
+      throw throwPromise(1000);
+    }
+    return <p>Hello, World!!</p>;
+  };
+
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <p>Test Page</p>
-      <AlwaysSuspend />
+      <h2>コンポーネント読み込みテスト</h2>
+      <Suspense fallback={<p>悪質な広告ページ</p>}>
+        {/* <AlwaysSuspend /> */}
+        <SometimesSuspend />
+        <button onClick={() => setCount((c) => c + 1)}>{count}</button>
+      </Suspense>
     </>
   );
 });
