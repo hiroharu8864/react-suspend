54094f34ac8a55ec4817fc7d167242f97eaab646
Hiroharu Tanaka <hiroharu8864@gol.com>
2022-07-13 15:37:36 +0900

    add sleep

--- src/components/pages/TestPage.tsx
+++ src/components/pages/TestPage.tsx
@@ -1,9 +1,17 @@
 import { FC, memo } from "react";
 
 export const TestPage: FC = memo(() => {
+  const sleep = (ms: number) => {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  };
+  const AlwaysSuspend: FC = () => {
+    throw sleep(1000);
+  };
+
   return (
     <>
       <p>Test Page</p>
+      <AlwaysSuspend />
     </>
   );
 });
