ca6b3498419dc8a64e115584bd602beaef0693e5
Hiroharu Tanaka <hiroharu8864@gol.com>
2022-07-13 17:09:02 +0900

    log追加

--- src/components/hooks/RenderingNotifier.tsx
+++ src/components/hooks/RenderingNotifier.tsx
@@ -1,6 +1,11 @@
 import { FC, memo } from "react";
 
+type Props = {
+  name: string;
+};
+
 export const RenderingNotifier: FC<Props> = memo((Props) => {
   const { name } = Props;
+  console.log(`${name} is rendered`);
   return null;
 });
